5e47d73933c697950a4fa4c52c77bfe461094722
Volodymyr <peganovvova@gmail.com>
2023-06-13 20:17:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Kapsys Pages Strapi
+
+This is a template for creating Kapsys Pages websites.
+
+## How to run locally
+1. Install dependencies: `npm i`
+2. Rename `.env.example` to `.env`
+3. Start development server: `npm run develop`
+
+## Create new KAPSYS pages Railway project
+[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/template/tEaHPe?referralCode=TjIptG)
---
+++ config/plugins.js
@@ -0,0 +1,39 @@
+module.exports = ({ env }) => ({
+    'users-permissions': {
+      config: {
+      jwtSecret: env('JWT_SECRET'),
+      },
+    },
+    graphql: {
+      enabled: true,
+      config: {
+        endpoint: '/graphql',
+  
+        defaultLimit: 25,
+        maxLimit: 100,
+  
+        apolloServer: {
+          tracing: true,
+        },
+      },
+    },
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
+          params: {
+            ACL: 'private',
+            Bucket: env('AWS_BUCKET'),
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+  });
